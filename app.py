454a898ca89964a44d32c483fc0faa490f08417f
XMarulandaH <143102196+XMarulandaH@users.noreply.github.com>
2023-08-24 09:53:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
 import streamlit as st
+from PIL import Image 
+
 st.title('La aplicación de Xiomarita') 
+
+st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
+st.write("Facilmente puedo realizar backend y frontend")
+image = Image.open('Gato-Memes-PNG')
+
+st.image(image, caption= 'Interfaces multimodales')
