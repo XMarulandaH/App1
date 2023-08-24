4bf7222e844531a40541d342c29eedfed98e9e4d
XMarulandaH <143102196+XMarulandaH@users.noreply.github.com>
2023-08-24 10:08:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,6 @@ st.write("Facilmente puedo realizar backend y frontend")
 image = Image.open('dedito.jpg')
 
 st.image(image, caption= 'Aqui va un dedito arriba')
+
+texto = st.text_input('Escribe algo', 'Este es mi texto')
+st.write('El texto escrito es', texto) 
